3537989d18a783f211429d0c5fa05a8791aea408
ManishS6 <contct.swami@yahoo.com>
2022-02-02 11:28:27 +0530

    Future components

---
+++ src/Components/SignIn.jsx
@@ -0,0 +1,17 @@
+// import '../App.css'
+
+// export default function SignIn(){
+    
+//     const signInWithGoogle = () => {
+//         const provider = new firebase.auth.GoogleAuthProvider();
+//         auth.signInWithPopup(provider);
+//     }
+
+//     return(
+//         <div className="SignIn">
+//             <h3>😇: manish.swami2019@vitstudent.ac.in</h3>
+//             <h3>👿: manish.swami@gmail.com</h3>
+//             <button onClick={signInWithGoogle}>Sign in with Google</button>
+//         </div>
+//     )
+// }
