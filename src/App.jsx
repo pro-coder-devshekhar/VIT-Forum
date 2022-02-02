17a083ed01343cb51f8b179053fc90646a1bc355
ManishS6 <contct.swami@yahoo.com>
2022-02-02 09:39:36 +0530

    create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Name from './Components/Name'
+import Contact from './Components/Contact'
+export default function App() {
+  return (
+    <div className="container">
+      <Name />
+      <Contact />
+    </div>
+  );
+}
---
+++ src/Components/Name.jsx
@@ -0,0 +1,10 @@
+import '../App.css';
+
+export default function Name() {
+  return (
+    <div>
+      <h2>Manish Swami</h2>
+      <br />
+    </div>
+  );
+}
